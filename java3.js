40d2ee4bcea87186e599df610cdfb9818c84ed71
venelin97 <venelin2805@gmail.com>
2025-12-17 13:10:49 -0600

    Update java3.js

--- java3.js
+++ java3.js
@@ -86,7 +86,7 @@ const extraQuestions = [
     options: ["Астро-Унгария", "Османската империя", "Персия"]
   }
 ];
-let quizQuestions = [extraQuestions];
+let quizQuestions = [...extraQuestions];
 
 
     
@@ -190,6 +190,7 @@ quizResultBtn.addEventListener("click", checkQuiz);
 
 
   
+
 
 
 
